18639ebc09b66a800a5d2b567f87504f42017c4a
josesg998 <josesg998@gmail.com>
2024-01-07 19:59:12 -0300

    pruebo reversion de funcion de formulario

--- forms/contact.php
+++ forms/contact.php
@@ -9,10 +9,20 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'josesg998@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = $receiving_email_address;
+    $from_name = $_POST['name'];
+    $from_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = "From: " . $from_name . "\nEmail: " . $from_email;
+  
+    $headers = "From: " . $from_name . "<" . $from_email . ">";
+  
+    if(mail($to, $subject, $message, $headers)) {
+      echo "Email sent successfully";
+    } else {
+      echo "Email sending failed";
+    }
   }
 
   $contact = new PHP_Email_Form;
